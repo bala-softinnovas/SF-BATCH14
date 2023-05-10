bf1e428b2635343c73230ab8de02679766f9298f
Balakrishna Ette <balakrishna.ette@outlook.com>
2023-05-10 09:09:16 +0530

    Update & Delete Record Functions

--- force-app/main/default/lwc/createRecordProduct/createRecordProduct.js
+++ force-app/main/default/lwc/createRecordProduct/createRecordProduct.js
@@ -1,17 +1,46 @@
 import { LightningElement } from 'lwc';
+import PRODUCT_OBJECT from '@salesforce/schema/EcommerceProduct__c';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class CreateRecordProduct extends LightningElement {
+    formdata = {};
 
     changeHandler(event) {
-
+        this.formdata[event.target.name] = event.target.value;
     }
 
     cancelHandler() {
-
+        this.template.querySelector('form.productform').reset();
+        this.template.querySelector('lightning-combobox').value = undefined;
     }
 
     saveHandler() {
-        
+        //prepare recordInput
+        const recordInput = {
+            apiName: PRODUCT_OBJECT.objectApiName,
+            fields: this.formdata
+        };
+
+        //create record
+        createRecord(recordInput)
+        .then(result => {
+            const toast = new ShowToastEvent({
+                title: "Success",
+                message: "product record has been created successfully!",
+                variant: "success"
+            });
+            this.dispatchEvent(toast);
+        })
+        .catch(error => {
+            console.error(error);
+            const toast = new ShowToastEvent({
+                title: error.detail.message,
+                message: error.detail.detail,
+                variant: "error"
+            });
+            this.dispatchEvent(toast);
+        })
     }
 
     get brandOptions() {
