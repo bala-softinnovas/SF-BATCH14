9a25476a8e50565005692eb448c96dbf42536362
Balakrishna Ette <balakrishna.ette@outlook.com>
2023-05-12 09:14:08 +0530

    Wired Apex

---
+++ force-app/main/default/lwc/imperativeApex1/imperativeApex1.js
@@ -0,0 +1,19 @@
+import getRecentLeads from '@salesforce/apex/LeadCtrl.getRecentLeads';
+import { LightningElement } from 'lwc';
+
+export default class ImperativeApex1 extends LightningElement {
+    leads;
+    error;
+
+    fetchLeads() {
+        getRecentLeads()
+            .then(result => {
+                console.log(result);
+                this.leads = result;
+            })
+            .catch(error => {
+                console.error(error);
+                this.error = error;
+            })
+    }
+}
